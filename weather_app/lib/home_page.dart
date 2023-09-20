7c8ce220badc4c2de873e1eb32289571972294ac
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-09-20 18:26:45 +0300

    htttp paketi eklendi ve api test edildi.(status code 200)

--- weather_app/lib/home_page.dart
+++ weather_app/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/screen_page.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,27 +11,41 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final String backgroundAssetUrl = 'assets/c.jpg';
+  double centigrate = 20.0;
+  String location = "Ankara";
+  final String apiKey = "bbe52cc5c96e0bc331ca6e56b4d64f2f";
+
+  //Apı'den gelen Weather datasını döner.
+  Future<http.Response> getWeatherData() {
+    return http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?units=metric&lang=tr&q=$location&appid=$apiKey'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          image:
-              DecorationImage(image: AssetImage(backgroundAssetUrl), fit: BoxFit.cover)),
+          image: DecorationImage(
+              image: AssetImage(backgroundAssetUrl), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('20 °C',
-                style: TextStyle(
+            ElevatedButton(onPressed: () async {
+             var response= await getWeatherData();
+             print(response.statusCode);
+            }, child: Text("TEST")),
+            Text("$centigrate°C",
+                style: const TextStyle(
                     fontSize: HomePageFontSize.centigradeSize,
                     fontWeight: FontWeight.bold)),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("Ankara",
-                    style: TextStyle(fontSize: HomePageFontSize.locationSize)),
+                Text(location,
+                    style: const TextStyle(
+                        fontSize: HomePageFontSize.locationSize)),
                 IconButton(
                     onPressed: () {
                       Navigator.push(
